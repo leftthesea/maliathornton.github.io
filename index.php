7a582d9bb30394e1291896ebbcab479a18bb112a
Malia <maliathornton@users.noreply.github.com>
2014-08-04 21:38:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,19 @@
 <head></head>
 
 <body>
-  <!-- PHP used to make the form functional -->
+  // Used for the contact form
 	<?php
 	    $name = $_POST['name'];
 	    $email = $_POST['email'];
 	    $comments = $_POST['comments'];
-	    $from = 'From: TangledDemo'; 
-	    $to = 'contact@tangledindesign.com'; 
-	    $subject = 'Hello';
+	    $from = 'From: Portfolio of Malia Thornton'; 
+	    $to = 'maliathornton@gmail.com'; 
+	    $subject = 'Hello!';
 	    $human = $_POST['human'];
 				
 	    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
 					
-	    if ($_POST['submit'] && $human == '4') {				 
+	    if ($_POST['submit']) {				 
 	        if (mail ($to, $subject, $body, $from)) { 
 		    print '<p>Your message has been sent!</p>';
 		} else { 
