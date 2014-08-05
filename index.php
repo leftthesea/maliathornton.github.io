edde8f835be38c72075e8832e918027bb64f1a56
Malia <maliathornton@users.noreply.github.com>
2014-08-04 21:34:22 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,26 @@
 <head></head>
 
 <body>
-<?php
-  // code
-?>
+  <!-- PHP used to make the form functional -->
+	<?php
+	    $name = $_POST['name'];
+	    $email = $_POST['email'];
+	    $comments = $_POST['comments'];
+	    $from = 'From: TangledDemo'; 
+	    $to = 'contact@tangledindesign.com'; 
+	    $subject = 'Hello';
+	    $human = $_POST['human'];
+				
+	    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+					
+	    if ($_POST['submit'] && $human == '4') {				 
+	        if (mail ($to, $subject, $body, $from)) { 
+		    print '<p>Your message has been sent!</p>';
+		} else { 
+		    print '<p>Something went wrong, go back and try again!</p>'; 
+		} 
+	    } 
+	?>
 
 </body>
 </html>
